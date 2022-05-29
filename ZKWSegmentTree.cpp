7da15956fb0b44637f31da92e26f92e607dc0cfb
PocaPoca <44687653+DearPoca@users.noreply.github.com>
2022-05-29 21:21:12 +0800

    修正线段树的bug

--- ZKWSegmentTree.cpp
+++ ZKWSegmentTree.cpp
@@ -14,74 +14,63 @@ public:
 #define ls node * 2 + 1  // left son
 #define rs node * 2 + 2  // right son
 
-    const static int MaxN = 1e5 + 7;
-    int n;
-    int tree[MaxN * 4];
+	const static int MaxN = 1e5 + 7;
+	int n;
+	int tree[MaxN * 4];
 
-    static inline int _Add(const int &a, const int &b) { return a + b; }
-    static inline int _Max(const int &a, const int &b) { return max(a, b); }
-    static inline int _Min(const int &a, const int &b) { return min(a, b); }
-    int (*SegmentTreeFunc)(const int &a, const int &b) = _Add;
+	static inline int _Add(const int& a, const int& b) { return a + b; }
+	static inline int _Max(const int& a, const int& b) { return max(a, b); }
+	static inline int _Min(const int& a, const int& b) { return min(a, b); }
+	int (*SegmentTreeFunc)(const int& a, const int& b) = _Add;
 
-    void Build(int a[], int n) {
-        this->n = n;
-        _Build(a, 0, n - 1, 0);
-    }
+	void Build(int a[], int n) {
+		this->n = n;
+		_Build(a, 0, n - 1, 0);
+	}
 
-    void Updata(int idx, int val) { _Updata(idx, val, 0, n - 1, 0); }
+	void Updata(int idx, int val) { _Updata(idx, val, 0, n - 1, 0); }
 
-    int Query(int L, int R) { return _Query(L, R, 0, n - 1, 0); }
+	int Query(int L, int R) { return _Query(L, R, 0, n - 1, 0); }
 
 private:
-    void _PushUp(int node) { tree[node] = SegmentTreeFunc(tree[ls], tree[rs]); }
+	void _PushUp(int node) { tree[node] = SegmentTreeFunc(tree[ls], tree[rs]); }
 
-    void _Build(int a[], int l, int r, int node) {
-        if (l == r) {
-            tree[node] = a[l];
-            return;
-        }
-        int mid = (l + r) / 2;
-        _Build(a, l, mid, ls);
-        _Build(a, mid + 1, r, rs);
-        _PushUp(node);  // 左右子树建好以后就可以求当前节点的值了
-    }
+	void _Build(int a[], int l, int r, int node) {
+		if (l == r) {
+			tree[node] = a[l];
+			return;
+		}
+		int mid = (l + r) / 2;
+		_Build(a, l, mid, ls);
+		_Build(a, mid + 1, r, rs);
+		_PushUp(node);  // 左右子树建好以后就可以求当前节点的值了
+	}
 
-    void _Updata(int idx, int val, int start, int end, int node) {
-        if (start == end) {
-            tree[node] = val;  // 修改树中的节点
-            return;
-        }
-        int mid = (start + end) / 2;
-        if (start <= idx && idx <= mid)
-            _Updata(idx, val, start, mid, ls);
-        else
-            _Updata(idx, val, mid + 1, end, rs);
-        _PushUp(node);
-    }
+	void _Updata(int idx, int val, int start, int end, int node) {
+		if (start == end) {
+			tree[node] = val;  // 修改树中的节点
+			return;
+		}
+		int mid = (start + end) / 2;
+		if (start <= idx && idx <= mid)
+			_Updata(idx, val, start, mid, ls);
+		else
+			_Updata(idx, val, mid + 1, end, rs);
+		_PushUp(node);
+	}
 
-    int _Query(int L, int R, int start, int end, int node) {
-        if (R < start || L > end)  // 没有交集
-            return 0;
-        else if (start == end)  // 区间缩小到1时
-            return tree[node];
-        else if (start <= L && R >= end)  // 完全在区间内
-            return tree[node];
-        int mid = (start + end) / 2;
+	int _Query(int L, int R, int start, int end, int node) {
+		if (R < start || L > end)  // 没有交集
+			return 0;
+		else if (start == end)  // 区间缩小到1时
+			return tree[node];
+		else if (L <= start && end <= R)  // 完全在区间内
+			return tree[node];
+		int mid = (start + end) / 2;
 
-        int ans = 0;
-        ans = SegmentTreeFunc(ans, _Query(L, R, start, mid, ls));
-        ans = SegmentTreeFunc(ans, _Query(L, R, mid + 1, end, rs));
-        return ans;
-    }
+		int ans = 0;
+		ans = SegmentTreeFunc(ans, _Query(L, R, start, mid, ls));
+		ans = SegmentTreeFunc(ans, _Query(L, R, mid + 1, end, rs));
+		return ans;
+	}
 };
-
-int main() {
-    vector<int> a = {34, 2, 34, 54, 6, 7, 4, 3, 23, 4, 4, 4};
-    SegmentTree st;
-    st.Build(a.data(), a.size());
-    cout << st.Query(0, 5) << endl;
-    st.Updata(1, 39);
-    cout << st.Query(0, 5) << endl;
-    st.Updata(3, 0);
-    cout << st.Query(0, 5) << endl;
-}
--- ZKWSegmentTreeSection.cpp
+++ ZKWSegmentTreeSection.cpp
@@ -3,70 +3,70 @@ public:
 #define ls node * 2 + 1  // 左儿子
 #define rs node * 2 + 2  // 右儿子
 
-    const static int MaxN = 1e5 + 7;
-    int n;
-    int tree[MaxN * 4], lazy[MaxN * 4];
+	const static int MaxN = 1e5 + 7;
+	int n;
+	int tree[MaxN * 4], lazy[MaxN * 4];
 
-    void PushUp(int node) { tree[node] = tree[ls] + tree[rs]; }
+	void PushUp(int node) { tree[node] = tree[ls] + tree[rs]; }
 
-    // node表示当前节点，ln,rn分别表示左右子树的节点数量
-    void PushDown(int node, int ln, int rn) {
-        if (lazy[node]) {
-            lazy[ls] += lazy[node];  // 下推标记
-            lazy[rs] += lazy[node];
-            tree[ls] += lazy[node] * ln;  // 修改子节点的Sum使之与对应的lazy相对应
-            tree[rs] += lazy[node] * rn;
-            lazy[node] = 0;  // 清除本结点标记
-        }
-    }
+	// node表示当前节点，ln,rn分别表示左右子树的节点数量
+	void PushDown(int node, int ln, int rn) {
+		if (lazy[node]) {
+			lazy[ls] += lazy[node];  // 下推标记
+			lazy[rs] += lazy[node];
+			tree[ls] += lazy[node] * ln;  // 修改子节点的Sum使之与对应的lazy相对应
+			tree[rs] += lazy[node] * rn;
+			lazy[node] = 0;  // 清除本结点标记
+		}
+	}
 
-    void _Build(int a[], int l, int r, int node) {
-        if (l == r) {
-            tree[node] = a[l];
-            return;
-        }
-        int mid = (l + r) / 2;
-        _Build(a, l, mid, ls);
-        _Build(a, mid + 1, r, rs);
-        PushUp(node);  // 左右子树建好以后就可以求当前节点的值了
-    }
+	void _Build(int a[], int l, int r, int node) {
+		if (l == r) {
+			tree[node] = a[l];
+			return;
+		}
+		int mid = (l + r) / 2;
+		_Build(a, l, mid, ls);
+		_Build(a, mid + 1, r, rs);
+		PushUp(node);  // 左右子树建好以后就可以求当前节点的值了
+	}
 
-    void Build(int a[], int n) {
-        this->n = n;
-        _Build(a, 0, n - 1, 0);
-    }
+	void Build(int a[], int n) {
+		this->n = n;
+		_Build(a, 0, n - 1, 0);
+	}
 
-    void _UpdateSection(int L, int R, int val, int start, int end, int node) {
-        if (L <= start && end <= R) {
-            tree[node] += val * (end - start + 1);  // 更新数字和，向上保持正确
-            lazy[node] += val;  // 增加lazy标记，表示本区间的Sum正确，子区间的Sum仍需要根据lazy的值来调整
-            return;
-        }
-        int mid = (start + end) / 2;
-        PushDown(node, mid - start + 1, end - mid);
-        if (L <= mid)  // 这里判断左右子树跟[L,R]有无交集，有交集才递归
-            _UpdateSection(L, R, val, start, mid, ls);
-        if (R > mid) _UpdateSection(L, R, val, mid + 1, end, rs);
-        PushUp(node);
-    }
+	void _UpdateSection(int L, int R, int val, int start, int end, int node) {
+		if (L <= start && end <= R) {
+			tree[node] += val * (end - start + 1);  // 更新数字和，向上保持正确
+			lazy[node] += val;  // 增加lazy标记，表示本区间的Sum正确，子区间的Sum仍需要根据lazy的值来调整
+			return;
+		}
+		int mid = (start + end) / 2;
+		PushDown(node, mid - start + 1, end - mid);
+		if (L <= mid)  // 这里判断左右子树跟[L,R]有无交集，有交集才递归
+			_UpdateSection(L, R, val, start, mid, ls);
+		if (R > mid) _UpdateSection(L, R, val, mid + 1, end, rs);
+		PushUp(node);
+	}
 
-    void UpdateSection(int L, int R, int val) { _UpdateSection(L, R, val, 0, n - 1, 0); }
+	void UpdateSection(int L, int R, int val) { _UpdateSection(L, R, val, 0, n - 1, 0); }
 
-    int _Query(int L, int R, int start, int end, int node) {
-        if (R < start || L > end)  // 没有交集
-            return 0;
-        else if (start == end)  // 区间缩小到1时
-            return tree[node];
-        else if (start <= L && R >= end)  // 完全在区间内
-            return tree[node];
-        int mid = (start + end) / 2;
+	int _Query(int L, int R, int start, int end, int node) {
+		if (R < start || L > end)  // 没有交集
+			return 0;
+		else if (start == end)  // 区间缩小到1时
+			return tree[node];
+		else if (L <= start && end <= R)  // 完全在区间内
+			return tree[node];
+		int mid = (start + end) / 2;
 
-        PushDown(node, mid - start + 1, end - mid);
-        int ans = 0;
-        ans += _Query(L, R, start, mid, ls);
-        ans += _Query(L, R, mid + 1, end, rs);
-        return ans;
-    }
+		PushDown(node, mid - start + 1, end - mid);
+		int ans = 0;
+		ans += _Query(L, R, start, mid, ls);
+		ans += _Query(L, R, mid + 1, end, rs);
+		return ans;
+	}
 
-    int Query(int L, int R) { return _Query(L, R, 0, n - 1, 0); }
+	int Query(int L, int R) { return _Query(L, R, 0, n - 1, 0); }
 };
